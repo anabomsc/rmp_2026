28c1e70995833c196ab3c926b06ffb10201384d2
anabomsc <ana.bomsc@discente.ufma.br>
2026-04-12 21:41:22 -0300

    optional joystick setup: config and launch files

---
+++ launch/joystick.launch.py
@@ -0,0 +1,47 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    # Launch arguments
+    declare_use_sim_time = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use sim time if true'
+    )
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    ld.add_action(declare_use_sim_time)
+
+    # Setup project paths
+    pkg_project = get_package_share_directory('rmp_2026')
+
+    # Setup namespaces
+    namespace = "/artbot"
+
+    # External joystick teleop setup
+    joy_params = os.path.join(pkg_project,'config','joystick.yaml')
+
+    joy_node = Node(
+        package='joy',
+        executable='joy_node',
+        parameters=[joy_params, {'use_sim_time': use_sim_time}], #
+    )
+    ld.add_action(joy_node)
+
+    teleop_node = Node(
+        package='teleop_twist_joy',
+        executable='teleop_node',
+        name='teleop_node',
+        parameters=[joy_params, {'use_sim_time': use_sim_time}],
+        remappings=[('/cmd_vel',f'{namespace}/cmd_vel')]
+    )
+    ld.add_action(teleop_node)
+
+    # Launch!
+    return ld      
